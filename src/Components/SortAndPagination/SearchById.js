79c276670c1054d5d7e57daaf3570c00747430b7
Bezll <jabez1992@hotmail.com>
2022-03-04 14:10:25 +0000

    Update SearchById.js

--- src/Components/SortAndPagination/SearchById.js
+++ src/Components/SortAndPagination/SearchById.js
@@ -1,46 +1,47 @@
-// import React, { useState } from "react";
-// import Form from "react-bootstrap/Form";
-// import FormControl from "react-bootstrap/FormControl";
-// import Button from "react-bootstrap/Button";
-// import { Link } from "react-router-dom";
-// import "./SearchById.css";
+/* eslint-disable */
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import "./SearchById.css";
 
-// const SearchById = () => {
-// 	const [searchInput, setSearchInput] = useState("");
+const SearchById = () => {
+	const [searchInput, setSearchInput] = useState("");
 
-// 	const handleSearch = (event) => {
-// 		setSearchInput(event.target.value);
-// 	};
+	const handleSearch = (event) => {
+		setSearchInput(event.target.value);
+	};
 
-// 	const handleSubmit = (event) => {
-// 		event.preventDefault();
-// 	};
-// 	return (
-// 		<div>
-// 			{/* <Link
-// 				style={{ textDecoration: "none" }}
-// 				to={`/reviews/${searchInput}`}
-// 			>
-// 				<Form onSubmit={handleSubmit} className="d-flex">
-// 					<FormControl
-// 						type="number"
-// 						placeholder="Search by Id"
-// 						className="me-2"
-// 						aria-label="Search"
-// 						onChange={handleSearch}
-// 						value={searchInput}
-// 					/>
-// 					<Button
-// 						className="search-btn"
-// 						variant="secondary"
-// 						size="sm"
-// 					>
-// 						Search
-// 					</Button>
-// 				</Form>
-// 			</Link> */}
-// 		</div>
-// 	);
-// };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+	};
+	return (
+		<div>
+			{/* <Link
+				style={{ textDecoration: "none" }}
+				to={`/reviews/${searchInput}`}
+			>
+				<Form onSubmit={handleSubmit} className="d-flex">
+					<FormControl
+						type="number"
+						placeholder="Search by Id"
+						className="me-2"
+						aria-label="Search"
+						onChange={handleSearch}
+						value={searchInput}
+					/>
+					<Button
+						className="search-btn"
+						variant="secondary"
+						size="sm"
+					>
+						Search
+					</Button>
+				</Form>
+			</Link> */}
+		</div>
+	);
+};
 
-// export default SearchById;
+export default SearchById;
